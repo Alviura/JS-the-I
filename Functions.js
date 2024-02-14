559e63b5e2c1cd26782460093f88f719eb586ac1
Alvin <alviura@outlook.com>
2024-02-14 20:22:14 +0300

    What JS can really do

---
+++ Functions.js
@@ -0,0 +1,10 @@
+/* Functions --> reusable block of code
+ *
+ * Built-in functions -> Known by the browser
+ * 
+ * The core client-side JavaScript language consists ofsome common programming features that allow you to do things like:
+ * 
+ * Store useful values inside variables.
+ * Operations on pieces of text (known as "strings" in programming). 
+ * Running code in response to certain events occurring on a web page. 
+ *
